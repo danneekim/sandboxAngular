398e3f924314d0e8101615c4b75b272ef182c240
Dannee Kim <dannee.kim.dev@gmail.com>
2023-01-25 10:03:41 -0500

    create login/logout functionality to header nav

--- angular-routing/src/app/app.component.ts
+++ angular-routing/src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { AuthService } from "./auth.service";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
 export class AppComponent {
-  title = 'cocktails-app-start';
+  title = "cocktails-app-start";
+  loggedIn: boolean;
+
+  constructor(private auth: AuthService) {}
+
+  logIn() {
+    this.auth.logIn();
+    this.loggedIn = true;
+  }
+
+  logOut() {
+    this.auth.logOut();
+    this.loggedIn = false;
+  }
 }
