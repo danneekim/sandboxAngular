70587a3f8b3dfc54a42d382ef47b4ee937409057
Dannee Kim <dannee.kim.dev@gmail.com>
2023-01-25 09:40:08 -0500

    fetch drink details and render view for cocktail details

--- angular-routing/src/app/cocktails-details/cocktails-details.component.ts
+++ angular-routing/src/app/cocktails-details/cocktails-details.component.ts
@@ -1,19 +1,33 @@
 import { Component, OnInit } from "@angular/core";
 import { ActivatedRoute } from "@angular/router";
+import { Observable } from "rxjs";
 import { switchMap } from "rxjs/operators";
+import { Cocktail } from "../cocktail.model";
+import { CocktailService } from "../cocktail.service";
 @Component({
   selector: "app-cocktails-details",
   templateUrl: "./cocktails-details.component.html",
   styleUrls: ["./cocktails-details.component.css"],
 })
 export class CocktailsDetailsComponent implements OnInit {
-  id: string;
+  // id: string;
+  cocktail$: Observable<Cocktail>;
 
-  constructor(private activeRoute: ActivatedRoute) {}
+  constructor(
+    private activeRoute: ActivatedRoute,
+    private cocktailService: CocktailService
+  ) {}
 
   ngOnInit(): void {
-    this.activeRoute.paramMap
-      .pipe(switchMap((paramsMap) => (this.id = paramsMap.get("id"))))
-      .subscribe();
+    // this.activeRoute.paramMap
+    //   .pipe(switchMap((paramsMap) => (this.id = paramsMap.get("id"))))
+    //   .subscribe();
+
+    this.cocktail$ = this.activeRoute.paramMap.pipe(
+      switchMap((paramsMap) => {
+        const id = paramsMap.get("id");
+        return this.cocktailService.getById(id);
+      })
+    );
   }
 }
